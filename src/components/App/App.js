4e421aca991ac063eea5cc6865a4a962f9eaa45a
mlizchap <mlizchap@gmail.com>
2018-09-22 13:19:13 -0400

    hooked up react-router and rendered App

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                APP
+            </div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App/App';
 
 ReactDOM.render(
-    <div>test</div>,
+    <Router>
+        <App />
+    </Router>,
     document.getElementById('root')
 )
