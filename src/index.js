1c6e51d1d66f4d17d8fe1ff317c988ca4d931f08
mlizchap <mlizchap@gmail.com>
2018-09-22 13:43:27 -0400

    redux hooked up to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App/App';
 import './index.scss';
 
+const store = createStore(() => {});
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 )
